457adb66801ec928663ab1c576b409b3c728c8b5
daxas-boop <daxas.swd@hotmail.com>
2021-03-17 02:22:32 -0300

    Adds Server Side Rendering

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,75 +1,13 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache';
-import { Provider, createClient, fetchExchange, dedupExchange } from 'urql';
-import { LoginMutation, MeDocument, MeQuery, RegisterMutation } from '../generated/graphql';
 import theme from '../theme';
-
-function updateQuery<Result, Query>(
-  cache: Cache,
-  qi: QueryInput,
-  result: any,
-  fn: (r: Result, q: Query) => Query
-) {
-  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
-}
-
-const client = createClient({
-  url: 'http://localhost:4000/graphql',
-  fetchOptions: {
-    credentials: 'include',
-  },
-  exchanges: [
-    dedupExchange,
-    cacheExchange({
-      updates: {
-        Mutation: {
-          login: (_result, args, cache, info) => {
-            updateQuery<LoginMutation, MeQuery>(cache, { query: MeDocument }, _result, (result, query) => {
-              if (result.login.errors) {
-                return query;
-              } else {
-                return {
-                  me: result.login.user,
-                };
-              }
-            });
-          },
-
-          register: (_result, args, cache, info) => {
-            updateQuery<RegisterMutation, MeQuery>(cache, { query: MeDocument }, _result, (result, query) => {
-              if (result.register.errors) {
-                return query;
-              } else {
-                return {
-                  me: result.register.user,
-                };
-              }
-            });
-          },
-
-          logout: (_result, args, cache, info) => {
-            updateQuery<LoginMutation, MeQuery>(cache, { query: MeDocument }, _result, () => ({ me: null }));
-          },
-        },
-      },
-    }),
-    fetchExchange,
-  ],
-});
 
 function MyApp({ Component, pageProps }: any) {
   return (
-    <Provider value={client}>
-      <ChakraProvider resetCSS theme={theme}>
-        <ColorModeProvider
-          options={{
-            useSystemColorMode: true,
-          }}
-        >
-          <Component {...pageProps} />
-        </ColorModeProvider>
-      </ChakraProvider>
-    </Provider>
+    <ChakraProvider resetCSS theme={theme}>
+      <ColorModeProvider options={{ useSystemColorMode: true }}>
+        <Component {...pageProps} />
+      </ColorModeProvider>
+    </ChakraProvider>
   );
 }
 
